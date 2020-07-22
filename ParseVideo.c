f4f8f105dacbdebf37e08a0574ab2a53dfd6284b
1037050764@qq.com <1037050764@qq.com>
2020-07-22 22:18:11 +0800

    add

--- ParseVideo.c
+++ ParseVideo.c
@@ -4,20 +4,28 @@
 //clang -g -o pvlib  ParseVideo.c  -lavformat -lavutil
 //打印当前目录下的文件，类似ls命令
 int showFile(){
-  int ret;
+  //设置输出日志级别
   av_log_set_level(AV_LOG_DEBUG);
-  av_log(NULL,AV_LOG_INFO,"hello word!\n");
+  int ret;
+  //调用FFMPEG函数删除文件
+  int r= avpriv_io_delete("test.db");
+  if(r<0){
+      av_log(NULL,AV_LOG_ERROR,"delete file flail\n");
+  }else{
+      av_log(NULL,AV_LOG_DEBUG,"delete file success\n");
+  }
+  //实现简单的ls命令功能（输出当前文件所在目录下的文件名称）
   AVIODirContext *ioCtx=NULL;
   AVIODirEntry * ioEntry=NULL;
   ret=avio_open_dir(&ioCtx,"./",NULL);
   if(ret<0){
-     av_log(NULL,AV_LOG_ERROR,"open dir fail %s /n",av_err2str(ret));
+     av_log(NULL,AV_LOG_ERROR,"open dir fail %s \n",av_err2str(ret));
      return -1;
   }
  
   av_log(NULL,AV_LOG_INFO,"----------current dir files info--------------\n");
   while(1){
-  avio_read_dir(ioCtx,&ioEntry);
+   ret= avio_read_dir(ioCtx,&ioEntry);
    if(ret<0){
     av_log(NULL,AV_LOG_ERROR,"read dir fail %s /n",av_err2str(ret));
     goto _fail; 
@@ -50,14 +58,15 @@ int printMateInfo(char* file){
 }
 
 int main(int argc,char* args[]){
-  if(argc<3){
-    av_log(NULL,AV_LOG_ERROR,"input agumet is error");
-  }
   showFile();
   char* src=args[1];
   char* des=args[2];
-  printMateInfo(src);
-  av_log(NULL,AV_LOG_DEBUG,"main()------>input arg:%s",args[1]);
+  if(argc<3){
+    av_log(NULL,AV_LOG_ERROR,"input agumet is error");
+  }else{
+    av_log(NULL,AV_LOG_DEBUG,"main()------>input arg:%s",args[1]);
+    printMateInfoprintMateInfo(src);
+  }
   return 0;
 }
 
